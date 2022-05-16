0ddee3a9ac7bb9a7f6a58564e47ff93caa02edbc
Pratheek <94678165+Pratheek681@users.noreply.github.com>
2022-05-16 06:17:36 +0000

    .

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -3,14 +3,22 @@
 largest = None
 smallest = None
 
-while True:
-    num = input("Enter a number? ")
 
+   
+while True:
+    num = input("Enter a number: ")
     if num == "done":
         break
+    num = int(num)
+    if largest is None or num>largest:
+        largest = num
+    elif smallest is None or num<smallest:
+        smallest = num
+   
 
-    # ...
 
-    print(num)
 
 print("Maximum", largest)
+print("minimum",smallest)
+
+
