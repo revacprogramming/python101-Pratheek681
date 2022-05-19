02e2b810522bd0ffda9e7731ce27aad9ced2f494
Pratheek <94678165+Pratheek681@users.noreply.github.com>
2022-05-19 09:32:12 +0000

    8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,15 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+total = 0
+for i in fh:
+    if not i.startswith("X-DSPAM-Confidence:"): 
+        continue
+    ipos = i.find(':')
+    flt = float(i[ipos + 1: ])
+    total= total + flt
+    count = count + 1
+
+print('Average spam confidence:', total/count)
